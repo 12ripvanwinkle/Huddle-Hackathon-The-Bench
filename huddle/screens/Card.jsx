31bd99b907fca86575c956632d57dcd2f31945ba
KxxngDemi <Demillebrom@gmail.com>
2026-03-14 15:43:05 -0500

    Card Component and friendscreen changes

---
+++ huddle/screens/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+    return (
+        <div className='card'>
+            <img src="https://via.placeholder.com/150" alt="Card Image" className='card-image'/>
+            <h2>FriendName</h2>
+            <p>This is a description of the card. It provides more details about the content.</p>
+            <button>View Profile</button>
+        </div>
+    );
+}
+
+
+
+export default Card;
---
+++ huddle/screens/FriendsScreen.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import{View, Text, StyleSheet, ScrollView,FlatList} from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+
+const DATA = [
+    {id: '1', name: 'John Doe'},
+    {id: '2', name: 'Jane Smith'},
+    {id: '3', name: 'Bob Johnson'}
+]
+const FriendsScreen = () => {
+    return (
+        <SafeAreaProvider style={{flexDirection: 'column'}}>
+            <View style = {{height: 50, backgroundColor: 'orange', alignItems: 'center', justifyContent: 'center'}}>
+                <Text style={styles.titleText}>Friend List</Text>
+            </View>
+            <ScrollView style={{margin: 10, padding: 10}}>
+                <Text style={styles.friendText}>John Doe</Text>
+                <Text style={styles.friendText}>Jane Smith</Text>
+                <Text style={styles.friendText}>Bob Johnson</Text>
+            </ScrollView>
+            <FlatList>
+                data={DATA}
+            </FlatList>
+        </SafeAreaProvider>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    titleText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        justifyContent: 'center'
+    },
+
+    friendText: {
+        fontSize: 18,
+        //marginVertical: 10
+    }
+})
+export default FriendsScreen;
